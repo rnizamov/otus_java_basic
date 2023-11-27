94a1366cfbbc2ab93db2c66f909e200f7297eed1
Rinat.Nizamov <_megion_@mail.ru>
2023-11-27 08:43:46 +0300

    Рефакторинг PhoneBook. Изменена реализация методов add, find, containsPhoneNumber.

--- src/main/java/ru/otus/javabasic/hw10/PhoneBook.java
+++ src/main/java/ru/otus/javabasic/hw10/PhoneBook.java
@@ -1,27 +1,41 @@
 package ru.otus.javabasic.hw10;
+
 import java.util.*;
 
 public class PhoneBook {
-    private static Map<Long, String> phoneNumbers = new HashMap<>();
+    private Map<String, List<String>> phoneNumbers = new HashMap<>();
 
-    public static void add(long number, String fio) {
-        phoneNumbers.put(number, fio);
-    }
-
-    public static Set<Long> find(String fio) {
-        Set<Long> set = new TreeSet<>();
-        for (Map.Entry<Long, String> entry : phoneNumbers.entrySet()) {
-            if (entry.getValue().equals(fio)) {
-               set.add(entry.getKey());
+    public void add(String fio, String number) {
+        if (containsPhoneNumber(number)) { // Проверка на дублирование номера телефона у разных Ф.И.О
+            System.out.println("Такой номер уже существует");
+        } else {
+            if (phoneNumbers.containsKey(fio)) {
+                phoneNumbers.get(fio).add(number);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(number);
+                phoneNumbers.put(fio, list);
             }
         }
-        return set;
     }
 
-    public static boolean containsPhoneNumber(long number) {
+    public List<String> find(String fio) {
+        if (phoneNumbers.containsKey(fio)) {
+            return phoneNumbers.get(fio);
+        } else {
+            System.out.println("Записи нет, добавьте номер телефона");
+            return new ArrayList<String>(); //todo думаю, что нужно передавать пустой List
+        }
+    }
+
+    public boolean containsPhoneNumber(String number) {
         boolean contains = false;
-        if (phoneNumbers.size() > 0) {
-            contains = phoneNumbers.containsKey(number);
+        Set<Map.Entry<String, List<String>>> set = phoneNumbers.entrySet();
+        Iterator<Map.Entry<String, List<String>>> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getValue().contains(number)) {
+                return true;
+            }
         }
         return contains;
     }
