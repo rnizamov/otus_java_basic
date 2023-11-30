d1be7fdb9bc472266d5f4ba827951cc5d8cecc45
Rinat.Nizamov <_megion_@mail.ru>
2023-11-30 20:04:44 +0300

    убрал лишнюю проверку

--- src/main/java/ru/otus/javabasic/hw1/HomeWork1.java
+++ src/main/java/ru/otus/javabasic/hw1/HomeWork1.java
@@ -25,7 +25,7 @@ public class HomeWork1 {
 
         if (data <= 10) {
             System.out.println("Красный");
-        } else if (data > 10 && data <= 20) {
+        } else if (data <= 20) {
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
