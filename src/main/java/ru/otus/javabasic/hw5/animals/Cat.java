d6c21e00695890d1ca9a1697426b95eca1fe655f
Rinat.Nizamov <_megion_@mail.ru>
2023-12-08 00:18:22 +0300

    homeWork5 update type variables

--- src/main/java/ru/otus/javabasic/hw5/animals/Cat.java
+++ src/main/java/ru/otus/javabasic/hw5/animals/Cat.java
@@ -1,7 +1,7 @@
 package ru.otus.javabasic.hw5.animals;
 
 public class Cat extends Animal {
-    public Cat(String name, int runSpeed, int swimSpeed, int stamina) {
+    public Cat(String name, float runSpeed, float swimSpeed, float stamina) {
         super(name, runSpeed, swimSpeed, stamina);
         swimFactor = 0;
     }
--- src/main/java/ru/otus/javabasic/hw5/animals/Dog.java
+++ src/main/java/ru/otus/javabasic/hw5/animals/Dog.java
@@ -1,11 +1,11 @@
 package ru.otus.javabasic.hw5.animals;
 
 public class Dog extends Animal {
-    public Dog(String name, int speedRun, int factorRun, int speedSwim, int factorSwim, int stamina) {
+    public Dog(String name, float speedRun, float factorRun, float speedSwim, float factorSwim, float stamina) {
         super(name, speedRun, factorRun, speedSwim, factorSwim, stamina);
     }
 
-    public Dog(String name, int runSpeed, int swimSpeed, int stamina) {
+    public Dog(String name, float runSpeed, float swimSpeed, float stamina) {
         super(name, runSpeed, swimSpeed, stamina);
         swimFactor = 2;
     }
