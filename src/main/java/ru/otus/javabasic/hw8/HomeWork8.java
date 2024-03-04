0b21c8a4fa2c90c8696e62dbd6442c2c0921566f
Rinat.Nizamov <_megion_@mail.ru>
2024-03-04 21:29:04 +0300

    update AppArraySizeException

--- src/main/java/ru/otus/javabasic/hw8/HomeWork8.java
+++ src/main/java/ru/otus/javabasic/hw8/HomeWork8.java
@@ -7,11 +7,11 @@ public class HomeWork8 {
     public static int sumAllElementsArraySize4x4(String[][] array) {
         int sum = 0;
         if (array.length != 4) {
-            throw new AppArraySizeException(4);
+            throw new AppArraySizeException(4, 4);
         }
         for (int i = 0; i < array.length; i++) {
             if (array[i].length != 4) {
-                throw new AppArraySizeException(4);
+                throw new AppArraySizeException(4, 4);
             }
         }
         for (int i = 0; i < array.length; i++) {
--- src/main/java/ru/otus/javabasic/hw8/exception/AppArraySizeException.java
+++ src/main/java/ru/otus/javabasic/hw8/exception/AppArraySizeException.java
@@ -2,7 +2,7 @@ package ru.otus.javabasic.hw8.exception;
 
 public class AppArraySizeException extends RuntimeException {
 
-    public AppArraySizeException(int count) {
-        super("Не верный размер массива != 4х4");
+    public AppArraySizeException(int i, int j) {
+        super("Не верный размер массива != " + i + "х" + j);
     }
 }
