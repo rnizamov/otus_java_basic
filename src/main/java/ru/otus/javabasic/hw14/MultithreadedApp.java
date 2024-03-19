0fb83015018b2219959bac96116e58b5b866a8ec
Rinat.Nizamov <_megion_@mail.ru>
2024-03-19 21:17:29 +0300

    add homeWork14

--- src/main/java/ru/otus/javabasic/hw14/MultithreadedApp.java
+++ src/main/java/ru/otus/javabasic/hw14/MultithreadedApp.java
@@ -19,21 +19,18 @@ public class MultithreadedApp {
     public static void fillArrayByMultiThreading() throws InterruptedException {
         long time1 = System.currentTimeMillis();
         double[] array = getDoubleArray(100_000_000);
-        Thread t1 = new Thread(() -> fillArray(array, 0, array.length / 4));
-        Thread t2 = new Thread(() -> fillArray(array, array.length / 4, array.length / 2));
-        Thread t3 = new Thread(() -> fillArray(array, array.length / 2, array.length *3/4));
-        Thread t4 = new Thread(() -> fillArray(array, array.length *3/4, array.length));
-
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-
-        t1.join();
-        t2.join();
-        t3.join();
-        t4.join();
-
+        Thread[] threadArray = {
+                new Thread(() -> fillArray(array, 0, array.length / 4)),
+                new Thread(() -> fillArray(array, array.length / 4, array.length / 2)),
+                new Thread(() -> fillArray(array, array.length / 2, array.length * 3 / 4)),
+                new Thread(() -> fillArray(array, array.length * 3 / 4, array.length))
+        };
+        for (Thread thread : threadArray) {
+            thread.start();
+        }
+        for (Thread thread : threadArray) {
+            thread.join();
+        }
         System.out.println("Время заполнения массива в 4 потока: " + (System.currentTimeMillis() - time1));
     }
 }
