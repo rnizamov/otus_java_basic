908a0e2ba087be8b9cfcb15003c2bf752dad4c0c
Rinat.Nizamov <_megion_@mail.ru>
2024-03-19 22:16:46 +0300

    add homeWork14

--- src/main/java/ru/otus/javabasic/hw14/MultithreadedApp.java
+++ src/main/java/ru/otus/javabasic/hw14/MultithreadedApp.java
@@ -4,33 +4,35 @@ import static ru.otus.javabasic.hw14.ArrayCreator.fillArray;
 import static ru.otus.javabasic.hw14.ArrayCreator.getDoubleArray;
 
 public class MultithreadedApp {
+    static final int SIZE_ARRAY = 100_000_000;
     public static void main(String[] args) throws InterruptedException {
-        fillArrayByOneThread();
-        fillArrayByMultiThreading();
+        fillArrayByOneThread(SIZE_ARRAY);
+        fillArrayByMultiThreading(SIZE_ARRAY, 4);
     }
 
-    public static void fillArrayByOneThread() {
+    public static void fillArrayByOneThread(int sizeArray) {
         long time1 = System.currentTimeMillis();
-        double[] array = getDoubleArray(100_000_000);
+        double[] array = getDoubleArray(sizeArray);
         fillArray(array, 0, array.length);
         System.out.println("Время заполнения массива в одном потоке: " + (System.currentTimeMillis() - time1));
     }
 
-    public static void fillArrayByMultiThreading() throws InterruptedException {
+    public static void fillArrayByMultiThreading(int sizeArray, int countThreads) throws InterruptedException {
         long time1 = System.currentTimeMillis();
-        double[] array = getDoubleArray(100_000_000);
-        Thread[] threadArray = {
-                new Thread(() -> fillArray(array, 0, array.length / 4)),
-                new Thread(() -> fillArray(array, array.length / 4, array.length / 2)),
-                new Thread(() -> fillArray(array, array.length / 2, array.length * 3 / 4)),
-                new Thread(() -> fillArray(array, array.length * 3 / 4, array.length))
-        };
+        double[] array = getDoubleArray(sizeArray);
+        int part = array.length / countThreads;
+        Thread[] threadArray = new Thread[countThreads];
+        for (int i = 0; i < countThreads; i++) {
+            int start = part * i;
+            int end = part * (i + 1);
+            threadArray[i] = new Thread(() -> fillArray(array, start, end));
+        }
         for (Thread thread : threadArray) {
             thread.start();
         }
         for (Thread thread : threadArray) {
             thread.join();
         }
-        System.out.println("Время заполнения массива в 4 потока: " + (System.currentTimeMillis() - time1));
+        System.out.println("Время заполнения массива в " + countThreads + " потока: " + (System.currentTimeMillis() - time1));
     }
 }
