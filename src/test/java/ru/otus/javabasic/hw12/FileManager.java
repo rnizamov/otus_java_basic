50a285611c374899610960722ea93d0f09fdfd48
Rinat.Nizamov <_megion_@mail.ru>
2024-04-01 21:02:12 +0300

    add homework 12

--- src/test/java/ru/otus/javabasic/hw12/FileManager.java
+++ src/test/java/ru/otus/javabasic/hw12/FileManager.java
@@ -1,6 +1,8 @@
 package ru.otus.javabasic.hw12;
 
-import java.io.File;
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,17 +14,76 @@ public class FileManager {
     private List<File> files = new ArrayList<>();
     private Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        FileManager fileManager = new FileManager();
-        fileManager.printFiles();
-    }
     public void start() {
         printFiles();
-
+        String fileName = askUserFileName();
+        if (fileName == null) return;
+        printFile(fileName);
+        String askUser = askUserToWorkWithFile();
+        if (askUser == null) return;
+        appendToFile(fileName, askUser);
+        printFile(fileName);
     }
 
-    private void askUser() {
-        System.out.println("Выберите файл для работы по номеру файла или :q для выхода");
+    private void printFile(String fileName) {
+        System.out.println("Содержание файла:");
+        System.out.println();
+        try (InputStreamReader in = new InputStreamReader(new FileInputStream(fileName))) {
+            int n = in.read();
+            while (n != -1) {
+                System.out.print((char) n);
+                n = in.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println();
+    }
+
+    private String askUserToWorkWithFile() {
+        System.out.println();
+        System.out.println("Введите текст который вы ходите добавить к файлу. :qa! для выхода");
+        System.out.println();
+        String response = scanner.nextLine();
+        if (response.equals(":qa!") || response.strip().equals("")) {
+            return null;
+        }
+        return response;
+    }
+
+    private void appendToFile(String fileName, String text) {
+        StringBuilder sb = new StringBuilder();
+        try (InputStreamReader in = new InputStreamReader(new FileInputStream(fileName))) {
+            int n = in.read();
+            while (n != -1) {
+                sb.append((char) n);
+                n = in.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(fileName))) {
+            sb.append(text);
+            byte[] buffer = sb.toString().getBytes(StandardCharsets.UTF_8);
+            for (int i = 0; i < buffer.length; i++) {
+                out.write(buffer[i]);
+                out.flush();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String askUserFileName() {
+        System.out.println("Выберите файл для работы или :qa! для выхода");
+        System.out.println();
+        String response = scanner.nextLine();
+        if (response.equals(":qa!") || response.strip().equals("")) {
+            return null;
+        }
+        return response;
     }
 
     private void printFiles() {
@@ -33,6 +94,7 @@ public class FileManager {
             for (int i = 0; i < files.size(); i++) {
                 System.out.println(i + 1 + " " + files.get(i).getName());
             }
+            System.out.println();
         }
     }
 }
