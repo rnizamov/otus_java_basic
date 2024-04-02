8c7db906a376ce5c52ced68128b1335b33db2197
Rinat.Nizamov <_megion_@mail.ru>
2024-04-02 17:30:09 +0300

    добавил javadoc

--- src/test/java/ru/otus/javabasic/hw12/FileManager.java
+++ src/test/java/ru/otus/javabasic/hw12/FileManager.java
@@ -1,7 +1,6 @@
 package ru.otus.javabasic.hw12;
 
 import java.io.*;
-import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,11 +8,30 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+/**
+ * @author Nizamov Rinat
+ * @version 1.0
+ * Main class for the application FileManager.
+ */
 public class FileManager {
+    /**
+     * This field indicating the root of the project.
+     */
     private final File file = new File(".");
+
+    /**
+     * List of text files located in the project root.
+     */
     private List<File> files = new ArrayList<>();
+
+    /**
+     * Object for dialogue with the user.
+     */
     private Scanner scanner = new Scanner(System.in);
 
+    /**
+     * This method start point of the application.
+     */
     public void start() {
         printFiles();
         String fileName = askUserFileName();
@@ -25,6 +43,10 @@ public class FileManager {
         printFile(fileName);
     }
 
+    /**
+     * This method prints the text content of the file and does not return the value.
+     * @param fileName String value. File name of the file to be printed to the console.
+     */
     private void printFile(String fileName) {
         System.out.println("Содержание файла:");
         System.out.println();
@@ -40,6 +62,10 @@ public class FileManager {
         System.out.println();
     }
 
+    /**
+     * A method for dialogue with the user asking to enter text to be added to a file or to exit.
+     * @return String value, text content or null.
+     */
     private String askUserToWorkWithFile() {
         System.out.println("\nВведите текст который вы ходите добавить к файлу. :qa! для выхода\n");
         String response = scanner.nextLine();
@@ -49,6 +75,11 @@ public class FileManager {
         return response;
     }
 
+    /**
+     * This method is for appending text to a file on a new line.
+     * @param fileName String file name text will be written.
+     * @param text text content to be written to the file.
+     */
     private void appendToFile(String fileName, String text) {
         text = "\n" + text;
         try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(fileName, true))) {
@@ -63,6 +94,10 @@ public class FileManager {
         }
     }
 
+    /**
+     * A method for dialogue with the user offering to select a file to work on or exit.
+     * @return String value, file name or null.
+     */
     private String askUserFileName() {
         System.out.println("Выберите файл для работы или :qa! для выхода");
         System.out.println();
@@ -73,6 +108,9 @@ public class FileManager {
         return response;
     }
 
+    /**
+     * The method prints a list of text files in the project root folder to the console and does not return the value.
+     */
     private void printFiles() {
         File[] listFiles = file.listFiles();
         files = Arrays.stream(listFiles).filter(File::isFile).collect(Collectors.toList());
