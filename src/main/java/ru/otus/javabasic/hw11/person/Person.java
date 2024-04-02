0f617df5d5711626d74fd81e04e15473711684ce
Rinat.Nizamov <_megion_@mail.ru>
2024-04-02 19:31:00 +0300

    homework11. part1.

--- src/main/java/ru/otus/javabasic/hw11/person/Person.java
+++ src/main/java/ru/otus/javabasic/hw11/person/Person.java
@@ -1,7 +1,49 @@
 package ru.otus.javabasic.hw11.person;
 
+import java.util.Objects;
+
 public class Person {
-    String name;
-    Position position;
-    Long id;
+    private String name;
+    private Position position;
+    private Long id;
+
+    public Person(String name, Position position, Long id) {
+        this.name = name;
+        this.position = position;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && Objects.equals(name, person.name) && position == person.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position, id);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", position=" + position +
+                ", id=" + id +
+                '}';
+    }
 }
